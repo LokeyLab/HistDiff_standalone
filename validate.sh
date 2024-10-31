cc187587b708ddf9ef5ca5a8af3e9f521e71338c
SauceBoss1 <dtvsworld@gmail.com>
2024-10-31 12:48:13 -0700

    works fine now to do a top to bottom full run

--- validate.sh
+++ validate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-time python histDiff.py
+# time python histDiff.py
 
-time python /home/derfelt/git_repos/HistDiffPipeline/pipelineCore/histdiff_pipelineV2.py -i "/home/derfelt/git_repos/HistDiff_standalone/temp_store/cellbycell/ff1301b8-94c2-11ee-ac86-02420a000112_cellbycell.tsv" -o "/home/derfelt/git_repos/HistDiff_standalone/temp_store/hd2.csv" -c "/home/derfelt/git_repos/HistDiff_standalone/temp_store/platemaps/SP7238PMA.csv" -cs; 
+#time python /home/derfelt/git_repos/HistDiffPipeline/pipelineCore/histdiff_pipelineV2.py -i "/home/derfelt/git_repos/HistDiff_standalone/temp_store/cellbycell/ff1301b8-94c2-11ee-ac86-02420a000112_cellbycell.tsv" -o "/home/derfelt/git_repos/HistDiff_standalone/temp_store/hd2.csv" -c "/home/derfelt/git_repos/HistDiff_standalone/temp_store/platemaps/SP7238PMA.csv" -cs; 
+
+time python histDiff.py -i "~/git_repos/HistDiff_standalone/temp_store/cellbycell/output.tsv" -o "~/git_repos/HistDiff_standalone/temp_store/official_run.csv" -c "~/git_repos/HistDiff_standalone/temp_store/platemaps/SP7238PMA.csv" -ref "sample_type" -wells "384_Well" -id "id" 
