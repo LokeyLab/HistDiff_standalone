510ae1b58e12c53a11298d0a0f98bdab5c88d4fc
SauceBoss1 <dtvsworld@gmail.com>
2024-11-26 14:22:34 -0800

    more writing

--- src/core/calc.rs
+++ src/core/calc.rs
@@ -8,6 +8,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 use std::path::Path;
+use std::usize;
 
 pub fn calculate_scores<P: AsRef<Path>>(
     cell_data: P,
@@ -36,6 +37,50 @@ pub fn calculate_scores<P: AsRef<Path>>(
         .delimiter(b'\t')
         .has_headers(true)
         .from_reader(reader);
+
+    let headers = csv_reader.headers()?.clone();
+
+    let id_col_idx: Vec<usize> = id_cols
+        .iter()
+        .map(|col| headers.iter().position(|h| h == col))
+        .collect::<Option<Vec<_>>>()
+        .ok_or("ID column not found in headers")?;
+
+    let feat_idx: Vec<usize> = features
+        .iter()
+        .filter_map(|feats| headers.iter().position(|h| h == feats))
+        .collect();
+
+    // the hisogram structure is as follows:
+    // histograms = {
+    //  well_id: {
+    //      feature: Histogram Struct
+    //      }
+    // }
+    let mut histograms: DashMap<String, DashMap<String, Hist1D>> = DashMap::new();
+
+    for res in csv_reader.records() {
+        let record = res?;
+
+        // in the event of multiple columns being the "id column"
+        // note: if the id column is multiple columns then they
+        // must be specified in the plate definition parameters
+        // with "_"s separating each value of each columns
+        let curr_well = id_cols
+            .iter()
+            .map(|id_feat| {
+                record
+                    .get(headers.iter().position(|h| h == id_feat).unwrap())
+                    .unwrap()
+            })
+            .collect::<Vec<&str>>()
+            .join("_");
+
+        if !plate_def.contains(&curr_well) {
+            continue;
+        }
+    }
+
     Ok(())
 }
 
